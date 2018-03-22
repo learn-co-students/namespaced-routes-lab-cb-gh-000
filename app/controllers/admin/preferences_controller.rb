560b5556f94e1b5b583fd7ce88cb622df2a3154b
platrol <takialog@gmail.com>
2018-03-22 13:51:10 +0800

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,6 +1,24 @@
 class Admin::PreferencesController < ApplicationController
 
   def index
+    if Preference.count == 0
+      @pref = Preference.create
+    else
+      @pref = Preference.last
+    end
 
   end
+
+  def update
+    @pref = Preference.last
+    @pref.update(post_params)
+    redirect_to admin_preferences_path
+  end
+
+  private
+
+  def post_params
+    params[:preference].permit(:song_sort_order, :artist_sort_order, :allow_create_artists, :allow_create_songs)
+  end
 end
+
